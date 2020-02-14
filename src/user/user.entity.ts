eac9f5a301fc5f3cb1aa084297ae94d370b725ef
Wanderson M <wandersonmaceds@gmail.com>
2020-02-14 14:26:44 -0300

    deixando metodo set password privado

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -29,7 +29,7 @@ export class User{
     /**
      * @param plainText set the password not encrypted.
      */
-    set password(plainText: string) {
+    private set password(plainText: string) {
         this._password = bcrypt.hashSync(plainText, bcrypt.genSaltSync());
     }
 }
