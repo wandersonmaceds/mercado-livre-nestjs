20d84bf66942f9d38525f57ea6da79cb865c4957
Wanderson M <wandersonmaceds@gmail.com>
2020-03-14 12:52:14 -0300

    criando rating de produto

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -6,12 +6,15 @@ import {
   Post,
   UseGuards,
   Request,
+  Param,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateProductDTO } from './dto/create-product.dto';
 import { Product } from './product.entity';
+import { CreateProductRatingDTO } from './dto/create-product-rating.dto';
+import { ProductRating } from './product-rating.entity';
 
 @UseGuards(AuthGuard('jwt'))
 @Controller('product')
@@ -19,6 +22,9 @@ export class ProductController {
   constructor(
     @InjectRepository(Product)
     private readonly productRepository: Repository<Product>,
+
+    @InjectRepository(ProductRating)
+    private readonly productRatingRepository: Repository<ProductRating>,
   ) {}
 
   @Post()
@@ -26,5 +32,18 @@ export class ProductController {
   async create(@Body() productDto: CreateProductDTO, @Request() request) {
     const product = productDto.toModel(request.user);
     this.productRepository.save(product);
+  }
+
+  @Post(':id/rating/')
+  @HttpCode(HttpStatus.OK)
+  async rate(
+    @Param('id') productId: number,
+    @Body() productRatingDto: CreateProductRatingDTO,
+    @Request() request,
+  ) {
+    const product = await this.productRepository.findOne(productId);
+    const productRating = productRatingDto.toModel(product, request.user);
+
+    this.productRatingRepository.save(productRating);
   }
 }
